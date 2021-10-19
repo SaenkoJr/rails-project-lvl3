3511e7141aaf3d1475e38897b5f3a4601f24ad92
Maksim Saenko <mm.saenko@gmail.com>
2021-10-20 00:06:51 +0300

    removed duplicates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
   def author_or_admin?(record)
     record.user.id == current_user.id || current_user.admin?
   end
+
+  def show_full_name_or_email(user)
+    user.full_name.empty? ? user.email : user.full_name
+  end
 end
